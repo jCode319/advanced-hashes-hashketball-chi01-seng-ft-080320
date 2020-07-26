fdf7cda019584049e387427b4d2920e5bbbb6703
Jarrell Jackson <jdjack3@gmail.com>
2020-07-26 02:27:28 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -168,7 +168,7 @@ def player_numbers(team_name)
   game_hash.each do |location, team_data|
   if team_name[:team_name] == team_name
     team_data[:players].each do |player|
-      jersey_numbers.push(:number)
+      jersey_numbers.push(number)
     end
   end
   end
